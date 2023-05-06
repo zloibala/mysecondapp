0b6dd268eb54d70fdf75092d2cb65d22f0c8328c
zloibala <boronova25@gmail.com>
2023-05-05 23:20:18 -0500

    RECT PROJECT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./components/largeHeader/Large.Header";
+import LargeHeader from "./components/largeHeader/Large.Header";
+import "./App.style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> My second App</h1>
+      <LargeHeader />
     </div>
   );
 }
-
 export default App;
---
+++ src/components/largeHeader/Large.Header.jsx
@@ -0,0 +1,11 @@
+import "./large.header.style.css";
+
+const LargeHeader = () => {
+  return (
+    <>
+      <h1> Large Header</h1>
+      <input type="password" max={14} />
+    </>
+  );
+};
+export default LargeHeader;
